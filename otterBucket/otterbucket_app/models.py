72d4f56a8e7aca9999b47f5c497e0a77d31409ea
enyaw15 <enyaw0215@gmail.com>
2021-03-08 17:26:43 -1000

    added bucketlist and bucketItem models
    	modified: otterbucket_app/models.py
    	bucketlist model has user id and bucket item id to track the relation
    	ship between a user and the items the user has
    	bucket item has information related to the bucket item
    	id so it can be tracked
    	item title is the title
    	item text is the description or elaboration of the item

--- otterBucket/otterbucket_app/models.py
+++ otterBucket/otterbucket_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BucketList(models.Model):
+    user_id = models.integerField
+    bucket_item_id = models.ForeignKey('BucketItem', on_delete=models.CASCADE)
+    
+class BucketItem(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    item_title = models.CharField(max_Length=200)
+    item_text = models.TextField()
