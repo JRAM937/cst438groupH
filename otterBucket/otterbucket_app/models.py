9c7b44eb61371e01df2676e91a2357e9735dacb7
enyaw15 <enyaw0215@gmail.com>
2021-03-08 17:31:32 -1000

    added user model 	modified:   otterbucket_app/models.py 	added a simple users model with a user_id and username

--- otterBucket/otterbucket_app/models.py
+++ otterBucket/otterbucket_app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class BucketList(models.Model):
-    user_id = models.integerField
-    bucket_item_id = models.ForeignKey('BucketItem', on_delete=models.CASCADE)
-    
+class user(models.Model):
+    userid = models.AutoField(primary_key = True)
+    username = models.CharField(max_Length=50)
+
 class BucketItem(models.Model):
     item_id = models.AutoField(primary_key=True)
     item_title = models.CharField(max_Length=200)
     item_text = models.TextField()
+    
+class BucketList(models.Model):
+    user_id = models.ForeignKey('user',on_delete=models.CASCADE)
+    bucket_item_id = models.ForeignKey('BucketItem', on_delete=models.CASCADE)
