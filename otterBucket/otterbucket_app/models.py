fe20a727e8590f747447d0f1b72936b49379d3b6
enyaw15 <enyaw0215@gmail.com>
2021-03-08 17:53:16 -1000

    added str methods to user, bucketItem, bucketlist 	modified:   otterbucket_app/models.py 	str methods to represent the items in tables: 	User 	BucketItem 	BucketList

--- otterBucket/otterbucket_app/models.py
+++ otterBucket/otterbucket_app/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
-    user_id = models.AutoField(primary_key = True)
-    username = models.CharField(max_Length=50)
+class User(models.Model):
+    id = models.AutoField(primary_key = True)
+    username = models.CharField(max_length=50)
+    def __str__(self):
+        return self.username
 
 class BucketItem(models.Model):
-    item_id = models.AutoField(primary_key=True)
-    item_title = models.CharField(max_Length=200)
-    item_text = models.TextField()
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    def __str__(self):
+        return self.title + ": " + self.text
     
 class BucketList(models.Model):
-    user_id = models.ForeignKey('user',on_delete=models.CASCADE)
-    bucket_item_id = models.ForeignKey('BucketItem', on_delete=models.CASCADE)
+    user = models.ForeignKey('user',on_delete=models.CASCADE)
+    bucket_item = models.ForeignKey('BucketItem', on_delete=models.CASCADE)
+    def __str__(self):
+        return "user: " + self.user_id + ", bucket item: " + self.bucket_item_id
