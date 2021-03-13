878f093f0744fa217a46e9e1f5af8f3c52519570
enyaw15 <enyaw0215@gmail.com>
2021-03-12 19:05:47 -1000

    reorganized file structure

--- otterBucket/settings.py
+++ otterBucket/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'otterbucket_app.apps.OtterbucketAppConfig',
+    'otterbucket_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
