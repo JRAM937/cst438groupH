585632d480bf55a0a80fee5452e70d6ab929d80a
enyaw15 <enyaw0215@gmail.com>
2021-03-12 15:01:14 -1000

    added requirements

--- otterBucket/otterBucket/settings.py
+++ otterBucket/otterBucket/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +82,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
