1eeb5e42ce67e38bc99a566a395443431d6c4288
UncertainTitle <wiwhite@csumb.edu>
2021-03-16 12:37:49 -0700

    Refactoring
    Refactoring for Heroku

---
+++ otterbucket_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from .models import BucketItem,BucketList,User
+
+# Create your views here.
+# index is the name used in urls.py to call this function
+def index(request):
+    return render(request, 'otterbucket_app/landing.html')
+
+def genBucketList(request):
+    for i in range(10):
+        b = BucketItem(title=i, text="text")
+        b.save()
+    return redirect('/list')
+
+def list(request):
+    bucketItems = BucketItem.objects.all()
+    context = {'bucketItems': bucketItems}
+    return render(request, 'otterbucket_app/display_list.html',context)
+
+def random_item(request):
+    return render(request, 'otterbucket_app/random_item.html')
