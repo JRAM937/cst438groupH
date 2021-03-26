df2f9f1cd6e883116bf2b7c0c5e894d70446566d
enyaw15 <enyaw0215@gmail.com>
2021-03-26 12:58:03 -1000

    fix heroku search-result

--- otterbucket_app/views.py
+++ otterbucket_app/views.py
@@ -214,7 +214,7 @@ def searchResult(request):
     query = Q(title__contains=search) | Q(text__contains=search)
     items = BucketItem.objects.filter(query)
     context['bucketItems'] = items
-    return render(request, 'otterBucket_app/search-result.html',context)
+    return render(request, 'otterbucket_app/search.html',context)
 
 
 def userList(request):
